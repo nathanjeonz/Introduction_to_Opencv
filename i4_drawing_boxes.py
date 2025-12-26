eef56eee333332dee14ae9cae8609c0249b9dae9
umair <eeumairali@gmail.com>
2025-12-26 17:07:00 +0500

    updated

--- i4_drawing_boxes.py
+++ i4_drawing_boxes.py
@@ -2,18 +2,19 @@ import cv2
 c = cv2.VideoCapture(0)
 print("cap passed",c)
 
-face = cv2.CascadeClassifier("apple.xml")
+face = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 while True:
     r,t = c.read()
-    cv2.imshow("web",t)
-
-    faces = face.detectMultiScale(t,1.3,5)
+    
+    gray = cv2.cvtColor(t,cv2.COLOR_BGR2GRAY)
+    faces = face.detectMultiScale(gray,1.3,5)
+    print(faces)
 
     for (x,y,w,h) in faces:
         cv2.rectangle(t,(x,y),(x+w,y+h),(0,255,0),2)
 
-
+    cv2.imshow("web",t)
 
     k = cv2.waitKey(1)
 
